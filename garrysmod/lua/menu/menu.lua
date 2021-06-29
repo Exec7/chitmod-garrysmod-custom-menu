a41a678608a523aa043fed3e554424311be99d0d
Exec <86654007+Exec7@users.noreply.github.com>
2021-06-29 12:00:57 +0300

    Update menu.lua

--- garrysmod/lua/menu/menu.lua
+++ garrysmod/lua/menu/menu.lua
@@ -1,3 +1,20 @@
+include( "mount/mount.lua" )
+include( "getmaps.lua" )
+include( "loading.lua" )
+include( "mainmenu.lua" )
+include( "video.lua" )
+include( "demo_to_video.lua" )
+
+include( "menu_save.lua" )
+include( "menu_demo.lua" )
+include( "menu_addon.lua" )
+include( "menu_dupe.lua" )
+include( "errors.lua" )
+include( "problems/problems.lua" )
+
+include( "motionsensor.lua" )
+include( "util.lua" )
+
 --This custom menu, with allowed functions in menustate https://wiki.facepunch.com/gmod/States#menu (gmod menu)
 if not MENU_DLL then return end
 timer.Simple(1,function()
